231e0a64590def078f82ce9e5a81afbfc4aca5d6
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-02-22 10:25:11 -0300

    Add files via upload

---
+++ calculadora de imc/README.md
@@ -0,0 +1,1 @@
+# calculando-imc
---
+++ calculadora/script/index.js
@@ -0,0 +1,28 @@
+function insert(num)
+        {
+           var numero = document.getElementById('resultado').innerHTML
+           document.getElementById('resultado').innerHTML = numero + num
+        }
+        function clean()
+        {
+            document.getElementById('resultado').innerHTML = "";
+        }
+        function back()
+        {
+            var  resultado = document.getElementById('resultado').innerHTML;
+            document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+        }
+        function conta()
+        {
+            var  resultado = document.getElementById('resultado').innerHTML;
+            if(resultado)
+            {
+                document.getElementById('resultado').innerHTML = eval(resultado)
+            }
+            else
+            {
+                document.getElementById('resultado').innerHTML= "0" 
+            }
+        }
+    
+        
