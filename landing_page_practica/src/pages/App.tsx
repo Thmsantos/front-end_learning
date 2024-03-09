2bad78cbd8ac27ca00554903025e751dd5437262
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-03-09 20:52:42 -0300

    Add files via upload

---
+++ landing_page_practica/src/pages/App.tsx
@@ -0,0 +1,11 @@
+ 
+function App() {
+
+  return (
+    <>
+     teste
+    </>
+  )
+}
+
+export default App
---
+++ landing_page_practica/src/syles/global.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const globalStyle = createGlobalStyle`
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    button {
+        background-color: none;
+        border: none;
+        outline: none;
+    }
+
+    a{
+        text-decoration:
+        outline: none;
+    }
+`
